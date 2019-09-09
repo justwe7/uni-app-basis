f752791bf65be4fc29d127531b2db43c47af9fed
justwe7 <x13133053566@163.com>
2019-09-09 10:04:52 +0800

    update

--- profile/dev_env.js
+++ profile/dev_env.js
@@ -1,4 +1,3 @@
 module.exports = {
-	// apiUrl: 'https://crouter.yunzongnet.com/xyscm-foodsafety-app/t3397/v2/api-docs/',// dev
-	apiUrl: 'https://crouter.yunzongnet.com/xyscm-foodsafety-app/t3397/',// 抓包的baseUrl
+	apiUrl: 'https://mock.api.io/',// dev
 }
